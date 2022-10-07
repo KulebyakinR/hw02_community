a31d603387bee31a9d92fa812682c69c781d246e
KulebyakinR <Kulebyakin.Ru@yandex.ru>
2022-10-07 21:43:35 +0300

    Исправил порядок import

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,5 @@
-
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from .models import Post
-from .models import Group
+from django.shortcuts import get_object_or_404, render
+from .models import Group, Post
 
 
 def index(request):
