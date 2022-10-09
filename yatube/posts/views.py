202ca1d05338ae517f0685fb3eb9670e49493539
KulebyakinR <Kulebyakin.Ru@yandex.ru>
2022-10-09 14:32:13 +0300

    добавил константу во views.py, добавил пропущений тэг

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
+POST_LIMIT = 10
+
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:POST_LIMIT]
     context = {
         'posts': posts,
     }
@@ -13,7 +15,7 @@ def index(request):
 
 def group_list(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:POST_LIMIT]
     context = {
         'group': group,
         'posts': posts,
