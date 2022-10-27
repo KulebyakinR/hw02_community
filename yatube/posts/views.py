db6953eded6983c12dfde5d69584b23b3c648645
KulebyakinR <Kulebyakin.Ru@yandex.ru>
2022-10-27 17:45:22 +0300

    Настроил пагинатор

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
@@ -6,18 +7,25 @@ POST_LIMIT = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:POST_LIMIT]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, POST_LIMIT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_list(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:POST_LIMIT]
+    posts = group.posts.all().order_by('-pub_date')
+    paginator = Paginator(posts, POST_LIMIT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
         'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
