7b10fb73393369f588a9834a55db28284bd59f0d
KulebyakinR <Kulebyakin.Ru@yandex.ru>
2022-10-29 16:47:26 +0300

    Добавил меню (обновил header)

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
+
+User = get_user_model()
 
 POST_LIMIT = 10
 
@@ -29,3 +32,27 @@ def group_list(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author__username=username)
+    paginator = Paginator(posts, POST_LIMIT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'posts': posts,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    posts = get_object_or_404(Post, id=post_id)
+    title = posts.text[:30]
+    context = {
+        "title": title,
+        "post": posts
+    }
+    return render(request, "posts/post_detail.html", context)
